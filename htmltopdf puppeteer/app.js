58a3319f627c3e5e20241feca78b7f2385090935
Tiruvengadam Lakshmanan <t.lakshmanan@perficient.com>
2019-07-23 12:56:21 +0530

    multi page pdf with header footer

--- htmltopdf puppeteer/app.js
+++ htmltopdf puppeteer/app.js
@@ -1,18 +1,30 @@
-const Puppeteer= require('puppeteer');
+const Puppeteer = require('puppeteer');
 
 
-(async ()=>{
-    const browser= await Puppeteer.launch();
-    const page= await browser.newPage();
-    await page.setViewport({width:1440,height:900,deviceScaleFactor:1});
-    await page.goto('https://medium.com/swlh/an-introduction-to-web-scraping-with-puppeteer-3d35a51fdca0', {waitUntil: 'networkidle2'});
+(async () => {
+    const browser = await Puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1440, height: 900, deviceScaleFactor: 1 });
+    await page.goto('http://www.thatjsdude.com/interview/css.html', { waitUntil: "networkidle2" });
+    console.log(await page.pages())
     await page.pdf({
-       
-        path:"./screen1.pdf",
-        pageRanges:"2",
-        format:"A4",
-        printBackground:true
-    
+
+        path: "./screen1.pdf",
+        displayHeaderFooter: true,
+        headerTemplate: '<span style="font-size: 30px; width: 50px; height: 50px; background-color: black; color:black; margin: 20px;">Header</span>',
+        footerTemplate: '<span style="font-size: 30px; width: 50px; height: 50px; background-color: black; color:black; margin: 20px;">Footer</span>',
+        pageRanges: " ",
+        format: "A4",
+        // scale: .54,
+        margin: {
+            top: '90px',
+            bottom: '150px',
+            right: '30px',
+            left: '30px',
+        },
+
+        printBackground: true
+
     })
     await browser.close();
 })();
